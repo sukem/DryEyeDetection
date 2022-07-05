a683371ac4d6dc44b531349b5597be834980cec5
taisuke matsushiro <tmfreq0825@gmail.com>
2022-07-05 12:30:22 +0900

    fragments

--- app/src/main/java/com/example/sukem/dryeyedetection/MainActivity.java
+++ app/src/main/java/com/example/sukem/dryeyedetection/MainActivity.java
@@ -23,7 +23,17 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 // ContentResolver dependency
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.common.collect.ImmutableSet;
 import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmark;
 import com.google.mediapipe.solutioncore.CameraInput;
@@ -40,228 +50,25 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
-    private FaceMesh facemesh;
-    // Run the pipeline and the model inference on GPU or CPU.
-    private static final boolean RUN_ON_GPU = true;
-
-    private static float lastUpdate = System.nanoTime();
-
-    private enum InputSource {
-        UNKNOWN,
-        IMAGE,
-        VIDEO,
-        CAMERA,
-    }
-    private InputSource inputSource = InputSource.UNKNOWN;
-    // Image demo UI and image loader components.
-    private ActivityResultLauncher<Intent> imageGetter;
-    private FaceMeshResultImageView imageView;
-    // Video demo UI and video loader components.
-    private VideoInput videoInput;
-    private ActivityResultLauncher<Intent> videoGetter;
-    // Live camera demo UI and camera components.
-    private CameraInput cameraInput;
-
-    private SolutionGlSurfaceView<FaceMeshResult> glSurfaceView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // TODO: Add a toggle to switch between the original face mesh and attention mesh.
-        imageView = new FaceMeshResultImageView(this);
-        setupLiveDemoUiComponents();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        NavHostFragment fragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        assert fragment != null;
+        NavController navController = fragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (inputSource == InputSource.CAMERA) {
-            // Restarts the camera and the opengl surface rendering.
-            cameraInput = new CameraInput(this);
-            cameraInput.setNewFrameListener(textureFrame -> facemesh.send(textureFrame));
-            glSurfaceView.post(this::startCamera);
-            glSurfaceView.setVisibility(View.VISIBLE);
-        } else if (inputSource == InputSource.VIDEO) {
-            videoInput.resume();
-        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (inputSource == InputSource.CAMERA) {
-            glSurfaceView.setVisibility(View.GONE);
-            cameraInput.close();
-        } else if (inputSource == InputSource.VIDEO) {
-            videoInput.pause();
-        }
-    }
-
-    /** Sets up the UI components for the live demo with camera input. */
-    private void setupLiveDemoUiComponents() {
-        Button startCameraButton = findViewById(R.id.button_start_camera);
-        startCameraButton.setOnClickListener(
-                v -> {
-                    if (inputSource == InputSource.CAMERA) {
-                        return;
-                    }
-                    stopCurrentPipeline();
-                    setupStreamingModePipeline(InputSource.CAMERA);
-                });
-    }
-
-    /** Sets up core workflow for streaming mode. */
-    private void setupStreamingModePipeline(InputSource inputSource) {
-        this.inputSource = inputSource;
-        // Initializes a new MediaPipe Face Mesh solution instance in the streaming mode.
-        facemesh =
-                new FaceMesh(
-                        this,
-                        FaceMeshOptions.builder()
-                                .setStaticImageMode(false)
-                                .setRefineLandmarks(true)
-                                .setRunOnGpu(RUN_ON_GPU)
-                                .build());
-        facemesh.setErrorListener((message, e) -> Log.e(TAG, "MediaPipe Face Mesh error:" + message));
-
-        if (inputSource == InputSource.CAMERA) {
-            cameraInput = new CameraInput(this);
-            cameraInput.setNewFrameListener(textureFrame -> facemesh.send(textureFrame));
-        } else if (inputSource == InputSource.VIDEO) {
-            videoInput = new VideoInput(this);
-            videoInput.setNewFrameListener(textureFrame -> facemesh.send(textureFrame));
-        }
-
-        // Initializes a new Gl surface view with a user-defined FaceMeshResultGlRenderer.
-        glSurfaceView =
-                new SolutionGlSurfaceView<>(this, facemesh.getGlContext(), facemesh.getGlMajorVersion());
-        glSurfaceView.setSolutionResultRenderer(new FaceMeshResultGlRenderer());
-        glSurfaceView.setRenderInputImage(true);
-        facemesh.setResultListener(
-                faceMeshResult -> {
-//                    logNoseLandmark(faceMeshResult, /*showPixelValues=*/ false);
-
-                    if (!faceMeshResult.multiFaceLandmarks().isEmpty()) {
-                        calcEyesAspectRatio(faceMeshResult.multiFaceLandmarks().get(0).getLandmarkList(),
-                                FaceMeshConnections.FACEMESH_LEFT_EYE);
-//                        Log.d(TAG, "FPS = " + String.valueOf(1000000000f / (System.nanoTime() - lastUpdate)));
-//                        lastUpdate = System.nanoTime();
-                    } else {
-//                        Log.d(TAG, "EYES NOT FOUND");
-                    }
-                    glSurfaceView.setRenderData(faceMeshResult);
-                    glSurfaceView.requestRender();
-                });
-
-        // The runnable to start camera after the gl surface view is attached.
-        // For video input source, videoInput.start() will be called when the video uri is available.
-        if (inputSource == InputSource.CAMERA) {
-            glSurfaceView.post(this::startCamera);
-        }
-
-        // Updates the preview layout.
-        FrameLayout frameLayout = findViewById(R.id.preview_display_layout);
-        imageView.setVisibility(View.GONE);
-        frameLayout.removeAllViewsInLayout();
-        frameLayout.addView(glSurfaceView);
-        glSurfaceView.setVisibility(View.VISIBLE);
-        frameLayout.requestLayout();
-    }
-
-    private float getDistance(NormalizedLandmark pt0, NormalizedLandmark pt1)
-    {
-        float x = pt0.getX() - pt1.getX();
-        float y = pt0.getY() - pt1.getY();
-        return (float) Math.sqrt(x*x + y*y);
-    }
-
-    private float getDistance(float[] vec1, float[] vec2)
-    {
-        float x = vec1[0] - vec2[0];
-        float y = vec1[1] - vec2[1];
-        return (float) Math.sqrt(x*x + y*y);
-    }
-
-    private float dotProduct(float[] vec1, float[] vec2)
-    {
-        return vec1[0]*vec2[0] + vec1[1]*vec2[1];
-    }
-
-    private void calcEyesAspectRatio(
-            List<NormalizedLandmark> faceLandmarkList,
-            ImmutableSet<FaceMeshConnections.Connection> connections)
-    {
-        if (connections.equals(FaceMeshConnections.FACEMESH_LEFT_EYE)) {
-            List<FaceMeshConnections.Connection> left = connections.asList();
-            NormalizedLandmark p1 = faceLandmarkList.get(left.get(0).start());
-            NormalizedLandmark p2 = faceLandmarkList.get(left.get(3).start());
-            NormalizedLandmark p3 = faceLandmarkList.get(left.get(4).end());
-            NormalizedLandmark p4 = faceLandmarkList.get(left.get(7).end());
-            NormalizedLandmark p5 = faceLandmarkList.get(left.get(12).end());
-            NormalizedLandmark p6 = faceLandmarkList.get(left.get(11).start());
-
-            float xyRatio = (float) Math.sqrt(1.0/1.61);
-            float[] horizontalVec = {(p4.getX() - p1.getX()) * xyRatio, p4.getY() - p1.getY()};
-            float[] orig = {0f, 0f};
-            float hvd = getDistance(horizontalVec, orig);
-            float[] verticalUnit = {horizontalVec[1] / hvd, -horizontalVec[0] / hvd};
-            float[] verticalVec1 = {(p2.getX() - p6.getX()) * xyRatio, p2.getY() - p6.getY()};
-            float[] verticalVec2 = {(p3.getX() - p5.getX()) * xyRatio, p3.getY() - p5.getY()};
-            float ear = (dotProduct(verticalVec1, verticalUnit) + dotProduct(verticalVec2, verticalUnit)) / (2 * hvd);
-
-            Log.d(TAG, "EAR = " + String.valueOf(ear));
-        } else if (connections.equals(FaceMeshConnections.FACEMESH_RIGHT_EYE)) {
-
-        }
-    }
-
-    private void startCamera() {
-        cameraInput.start(
-                this,
-                facemesh.getGlContext(),
-                CameraInput.CameraFacing.FRONT,
-                glSurfaceView.getWidth(),
-                glSurfaceView.getHeight());
-    }
-
-    private void stopCurrentPipeline() {
-        if (cameraInput != null) {
-            cameraInput.setNewFrameListener(null);
-            cameraInput.close();
-        }
-        if (videoInput != null) {
-            videoInput.setNewFrameListener(null);
-            videoInput.close();
-        }
-        if (glSurfaceView != null) {
-            glSurfaceView.setVisibility(View.GONE);
-        }
-        if (facemesh != null) {
-            facemesh.close();
-        }
-    }
-
-    private void logNoseLandmark(FaceMeshResult result, boolean showPixelValues) {
-        if (result == null || result.multiFaceLandmarks().isEmpty()) {
-            return;
-        }
-        NormalizedLandmark noseLandmark = result.multiFaceLandmarks().get(0).getLandmarkList().get(1);
-        // For Bitmaps, show the pixel values. For texture inputs, show the normalized coordinates.
-        if (showPixelValues) {
-            int width = result.inputBitmap().getWidth();
-            int height = result.inputBitmap().getHeight();
-            Log.i(
-                    TAG,
-                    String.format(
-                            "MediaPipe Face Mesh nose coordinates (pixel values): x=%f, y=%f",
-                            noseLandmark.getX() * width, noseLandmark.getY() * height));
-        } else {
-            Log.i(
-                    TAG,
-                    String.format(
-                            "MediaPipe Face Mesh nose normalized coordinates (value range: [0, 1]): x=%f, y=%f",
-                            noseLandmark.getX(), noseLandmark.getY()));
-        }
     }
 }
